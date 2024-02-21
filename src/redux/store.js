c4201481ced1f8d6a1f3f49104251d4a4bb0bd70
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-21 16:37:23 +0200

    Correct using exported name 'store' as identifier for default export

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import greetingsReducer from './greetings/greetingsSlice';
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     greetings: greetingsReducer,
   },
