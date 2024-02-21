aa88b483526f270e87ec2d42b51a5a09eb2bf47e
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-21 12:33:32 +0200

    Add Slice reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import greetingsReducer from './greetings/greetingsSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    greetings: greetingsReducer,
+  },
 });
 
 export default store;
