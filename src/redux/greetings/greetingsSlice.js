0c0cc926df1ff30d654bf877689bfd32fc9abe73
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-21 12:48:55 +0200

    Add extra reducers

--- src/redux/greetings/greetingsSlice.js
+++ src/redux/greetings/greetingsSlice.js
@@ -24,6 +24,22 @@ const greetingsSlice = createSlice({
   name: 'greetings',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchRandomGreeting.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(fetchRandomGreeting.fulfilled, (state, action) => {
+        state.greeting = action.payload;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(fetchRandomGreeting.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
+  },
 });
 
 export default greetingsSlice.reducer;
