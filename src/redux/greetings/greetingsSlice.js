d7dee3941e760da6295cde76cb6f93e658e38285
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-21 12:42:40 +0200

    Fetch data from API with AsyncThunk

--- src/redux/greetings/greetingsSlice.js
+++ src/redux/greetings/greetingsSlice.js
@@ -1,4 +1,18 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const greetingsUrl = 'http://localhost:3000/greetings/random';
+
+export const fetchRandomGreeting = createAsyncThunk(
+  'greeting/fetchRandomGreeting',
+  async () => {
+    const response = await fetch(greetingsUrl);
+    if (!response.ok) {
+      throw new Error('Unable to fetch');
+    }
+    const data = await response.json();
+    return data.greeting;
+  },
+);
 
 const initialState = {
   greeting: '',
@@ -9,6 +23,7 @@ const initialState = {
 const greetingsSlice = createSlice({
   name: 'greetings',
   initialState,
+  reducers: {},
 });
 
 export default greetingsSlice.reducer;
